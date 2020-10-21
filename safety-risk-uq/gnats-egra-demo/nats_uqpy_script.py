bff483ec6614915192b1b498a1c231a829dad08a
John McFarland <john.mcfarland@swri.org>
2020-10-21 14:04:11 -0500

    Initial commit of all files for gnats-egra demo

---
+++ safety-risk-uq/gnats-egra-demo/nats_uqpy_script.py
@@ -0,0 +1,17 @@
+# Script to handle running NATS for UQpy
+
+import os
+import sys
+import subprocess
+import fire
+
+def model(sample_index):
+
+    input_file = os.path.join('InputFiles','nats_input_{}.txt'.format(sample_index))
+    output_file = 'nats_output_{}.txt'.format(sample_index)
+
+    # sys.executable refers to the current Python executable
+    subprocess.run([sys.executable, 'nats_driver.py', input_file, output_file])
+    
+if __name__ == '__main__':
+   fire.Fire(model)
