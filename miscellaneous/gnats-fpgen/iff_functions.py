f37b0a62ef00cda7202a24de165c1e02a64f76e1
Ghadir Haikal <ghadir.haikal@swri.org>
2021-04-06 12:39:40 -0500

    adding trx files with 'closest gate' selections - correction

--- miscellaneous/gnats-fpgen/iff_functions.py
+++ miscellaneous/gnats-fpgen/iff_functions.py
@@ -205,8 +205,9 @@ def get_departure_gate_and_rwy_from_iff(iff_data,callsign,gnatsSim,departureAirp
     else:
         gateOpts = gnatsSim.airportInterface.getAllGates(departureAirport)
         gateOpts = [opt for opt in gateOpts if opt.lower().startswith('gate')]
-        dep_gate = random.choice(gateOpts)
-
+        rwy_node = gnatsSim.airportInterface.getRunwayEnds(departureAirport,rwy_name)[1]
+        dep_gate = get_adjacent_node_closer_to_runway(gateOpts,rwy_node,departureAirport)
+        
     return dep_gate,rwy_name
 
 
