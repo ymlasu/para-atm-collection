06ee48cea5053e3634723b620bf3bdaa5a7973eb
Jueming6 <38963381+Jueming6@users.noreply.github.com>
2021-07-14 18:27:08 -0700

    Add files via upload

---
+++ safety-risk-uq/UAS_ObstacleAvoidance_SafetyBound_RL/plotting.py
@@ -0,0 +1,34 @@
+import matplotlib
+import numpy as np
+import pandas as pd
+from collections import namedtuple
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+EpisodeStats = namedtuple("Stats",["episode_lengths", "episode_rewards"])
+
+def plot_episode_stats(stats, smoothing_window=10, noshow=False):
+    # Plot the episode length over time
+    fig1 = plt.figure(figsize=(10,5))
+    plt.plot(stats.episode_lengths)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Length")
+    plt.title("Episode Length over Time")
+
+    # Plot the episode reward over time
+    fig2 = plt.figure(figsize=(10,5))
+    rewards_smoothed = pd.Series(stats.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+    plt.plot(rewards_smoothed)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Reward (Smoothed)")
+    plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
+
+    # Plot time steps and episode number
+    fig3 = plt.figure(figsize=(10,5))
+    plt.plot(np.cumsum(stats.episode_lengths), np.arange(len(stats.episode_lengths)))
+    plt.xlabel("Time Steps")
+    plt.ylabel("Episode")
+    plt.title("Episode per time step")
+    plt.show()
+
+    return fig1, fig2, fig3
