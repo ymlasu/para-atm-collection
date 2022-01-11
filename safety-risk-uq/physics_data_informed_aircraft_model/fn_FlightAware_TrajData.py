f682bfa7299898081b6bb91346e213b931a31806
absubr <46486177+absubr@users.noreply.github.com>
2022-01-11 15:08:33 -0600

    Add files via upload

--- safety-risk-uq/physics_data_informed_aircraft_model/fn_FlightAware_TrajData.py
+++ safety-risk-uq/physics_data_informed_aircraft_model/fn_FlightAware_TrajData.py
@@ -16,58 +16,68 @@ def pyfn_FlightAware_trajectory_extractor(FA_URL):
     
     cols = traj.columns
     col0=traj.get(cols[0])
-    Ix_dep_1 = col0.str.contains("Departure")
-    Ix_arr_1 = col0.str.contains("Arrival")
+    #Ix_dep_1 = tmp1.str.contains("Departure")
+    #Ix_arr_1 = tmp1.str.contains("Arrival")
     
-    Ix_dep=[i for i, x in enumerate(Ix_dep_1) if x]
-    Ix_arr=[i for i, x in enumerate(Ix_arr_1) if x]
+    #Ix_dep=[i for i, x in enumerate(Ix_dep_1) if x]
+    #Ix_arr=[i for i, x in enumerate(Ix_arr_1) if x]
     
     temp1= datetime.datetime.strptime(col0[5][:15],"%a %I:%M:%S %p")
     temp2= datetime.datetime.strptime(col0[6][:15],"%a %I:%M:%S %p")
     duration=temp2-temp1
     duration.total_seconds()
     
-    col01=col0[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col01=col0[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col01=col0[1:]
     col02=col01.str.slice(0,15)
     
     col03=pd.to_datetime(col02,format='%a %I:%M:%S %p')
-    col04=col03-col03[Ix_dep[0]+1]
+    #col04=col03-col03[Ix_dep[0]+1]
+    col04=col03-col03[1]
     
     di = {'Mon':0, 'Tue':1, 'Wed':2, 'Thu':3, 'Fri':4, 'Sat':5, 'Sun':6}
     temp=col01.str.slice(0,3)
     seconds_offset = temp.replace(di)*24*60*60
     
-    col05 = col04.dt.total_seconds() + seconds_offset - seconds_offset[Ix_dep[0]+1]
+    #col05 = col04.dt.total_seconds() + seconds_offset - seconds_offset[Ix_dep[0]+1]
+    col05 = col04.dt.total_seconds() + seconds_offset - seconds_offset[1]
     
     # Latitude
     col1=traj.get(cols[1])
-    col11=col1[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col11=col1[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col11=col1[1:]
     col12=col11.str.extract("^(\\-?\d+\.\d{4}?)") # regex extracts the following: from start opf string (^) with possible -ve (\\-?) unlimited digits in series (\d+) a decimal point (\.) four digits \d{4} 
     
     # Longitude
     col2=traj.get(cols[2])
-    col21=col2[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col21=col2[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col21=col2[1:]
     col22=col21.str.extract("^(\\-?\d+\.\d{4}?)")
     col22.astype('float')
     
     # Course
     col3=traj.get(cols[3])
-    col31=col3[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col31=col3[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col31=col3[1:]
     col32=col31.str.extract("(\\-?\d+)")
     
     # TAS kts
     col4=traj.get(cols[4])
-    col41=col4[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col41=col4[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col41 = col4[1:]
     col42=col41.str.extract("(\\-?\d+)")
     
     # TAS mph
     col5=traj.get(cols[5])
-    col51=col5[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col51=col5[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col51=col5[1:]
     col52=col51.str.extract("(\\-?\d+)")
     
     # Altitude ft
     col6=traj.get(cols[6])
-    col61=col6[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col61=col6[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col61=col6[1:]
+    col61 = col61.replace(np.nan,0)
     temp=col61.astype(np.int64)
     temp1=temp.astype(str)
     len1= temp1.str.len()
@@ -76,14 +86,15 @@ def pyfn_FlightAware_trajectory_extractor(FA_URL):
     
     # ROCD
     col7=traj.get(cols[7])
-    col71=col7[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    #col71=col7[(Ix_dep[0]+1):(Ix_arr[0]-1)]
+    col71=col7[1:]
     col72=col71.fillna(0)
     
     names=['Time_s','Latitude','Longitude','Course_deg','TAS_kts','TAS_mph','Altitude_ft','ROCD']
     traj_mat = pd.concat([col05,col12,col22,col32,col42,col52,col62,col72], axis=1,ignore_index = True)
     traj_mat.columns = names
     traj_mat.index = np.arange(traj_mat.shape[0])
-
+    
     Time_s = np.array(col05)
     Latitude = np.array(col12)
     Longitude = np.array(col22)
--- safety-risk-uq/physics_data_informed_aircraft_model/fn_flight_phases.py
+++ safety-risk-uq/physics_data_informed_aircraft_model/fn_flight_phases.py
@@ -21,7 +21,7 @@ def pyfn_flight_phase(ROCD, Altitude_ft, Time_s):
     temp2=temp.reshape(-1, 1)
     
     sse=[]
-    for kmeans_k in range(1,10):
+    for kmeans_k in range(1,5):
         kmeans = KMeans(
             init='k-means++',
             n_clusters=kmeans_k,
@@ -30,8 +30,13 @@ def pyfn_flight_phase(ROCD, Altitude_ft, Time_s):
         )
         kmeans.fit(temp2)
         sse = np.append(sse,kmeans.inertia_)
-    first_zero_in_sse = np.where(sse == 0)[0][0]
-    n_clusters_cruise = first_zero_in_sse
+    if sse.size==np.count_nonzero(sse):
+        # find elbow
+        rel_diff = np.divide(sse[1:]-sse[:-1],sse[:-1])
+        n_clusters_cruise = 1 + np.argmin(rel_diff)
+    else: 
+        first_zero_in_sse = np.where(sse == 0)[0][0]
+        n_clusters_cruise = first_zero_in_sse
     
     kmeans = KMeans(
         init='k-means++',
@@ -59,52 +64,61 @@ def pyfn_flight_phase(ROCD, Altitude_ft, Time_s):
     # 3. Formatting descent phase
     ROCD_descent_phase = ROCD[Ix_ToD:]
     ROCD_neutral_descent = np.where(abs(ROCD_descent_phase)<ROCD_tol)[0]
-    Alt_neutral_descent = Altitude_ft[EndOfCruise+ROCD_neutral_descent]
-    
-    temp3=Alt_neutral_descent.reshape(-1, 1)
-    
-    sse=[]
-    for kmeans_k in range(1,10):
-        kmeans = KMeans(
-            init='k-means++',
-            n_clusters=kmeans_k,
-            n_init=10,
-            max_iter=300,
-        )
-        kmeans.fit(temp3)
-        sse = np.append(sse,kmeans.inertia_)
-    first_zero_in_sse = np.where(sse == 0)[0][0]
-    n_clusters_descent = first_zero_in_sse
-    
-    verify_length = []
-    for ix in range(1,first_zero_in_sse):
-        kmeans = KMeans(
-            init='k-means++',
-            n_clusters=ix,
-            n_init=10,
-            max_iter=300,
-        )
-        kmeans.fit(temp3)
-    
-        clusters_with_adequte_length = 0
-        for jx in range(n_clusters_descent):
-            indices_cluster_1 = np.where(kmeans.labels_==jx)[0]
-            clusters_with_adequte_length = clusters_with_adequte_length+(indices_cluster_1.size>5)
-        verify_length.append(clusters_with_adequte_length==ix)
-    if sum(verify_length)==0:
-        valid_number_of_clusters = 0
+    if ROCD_neutral_descent.size==0:
         hold_descent_altitudes = []
-    else:
-        valid_number_of_clusters = 1 + max(np.where(verify_length)[0])
+    else: 
+        Alt_neutral_descent = Altitude_ft[EndOfCruise+ROCD_neutral_descent]
         
-        kmeans = KMeans(
-            init='k-means++',
-            n_clusters=valid_number_of_clusters,
-            n_init=10,
-            max_iter=300,
-        )
-        kmeans.fit(temp3)
-        hold_descent_altitudes = kmeans.cluster_centers_[:,0]
+        temp3=Alt_neutral_descent.reshape(-1, 1)
+        
+        sse=[]
+        for kmeans_k in range(1,10):
+            kmeans = KMeans(
+                init='k-means++',
+                n_clusters=kmeans_k,
+                n_init=10,
+                max_iter=300,
+            )
+            kmeans.fit(temp3)
+            sse = np.append(sse,kmeans.inertia_)
+        
+        if sse.size==np.count_nonzero(sse):
+            # find elbow
+            rel_diff = np.divide(sse[1:]-sse[:-1],sse[:-1])
+            n_clusters_descent = 1 + np.argmin(rel_diff)
+        else: 
+            first_zero_in_sse = np.where(sse == 0)[0][0]
+            n_clusters_descent = first_zero_in_sse
+        
+        verify_length = []
+        for ix in range(1,first_zero_in_sse):
+            kmeans = KMeans(
+                init='k-means++',
+                n_clusters=ix,
+                n_init=10,
+                max_iter=300,
+            )
+            kmeans.fit(temp3)
+        
+            clusters_with_adequte_length = 0
+            for jx in range(n_clusters_descent):
+                indices_cluster_1 = np.where(kmeans.labels_==jx)[0]
+                clusters_with_adequte_length = clusters_with_adequte_length+(indices_cluster_1.size>5)
+            verify_length.append(clusters_with_adequte_length==ix)
+        if sum(verify_length)==0:
+            valid_number_of_clusters = 0
+            hold_descent_altitudes = []
+        else:
+            valid_number_of_clusters = 1 + max(np.where(verify_length)[0])
+            
+            kmeans = KMeans(
+                init='k-means++',
+                n_clusters=valid_number_of_clusters,
+                n_init=10,
+                max_iter=300,
+            )
+            kmeans.fit(temp3)
+            hold_descent_altitudes = kmeans.cluster_centers_[:,0]
 
 
     return Ix_ToC, Ix_ToD, time_cluster_all, alt_cluster_all, hold_descent_altitudes
